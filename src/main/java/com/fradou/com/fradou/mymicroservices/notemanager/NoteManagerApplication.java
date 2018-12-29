9accc884bd7c606c4e456c8e590ba1d9e54333b2
Fradou <alex.fradet.1@gmail.com>
2018-12-29 16:40:48 +0100

    Init project

---
+++ src/main/java/com/fradou/com/fradou/mymicroservices/notemanager/NoteManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fradou.com.fradou.mymicroservices.notemanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class NoteManagerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
